6b713fe7bd4da64499764cf663f5f0eae4b22f5b
8tomat8 <8tomat8@gmail.com>
2017-09-09 22:43:48 +0300

    Update attachments.py

--- helpers/attachments.py
+++ helpers/attachments.py
@@ -41,21 +41,21 @@ class Attachments:
 
     async def photo(self, data: dict, session):
         path = 'cache/photos/'
-        {'access_key': 'd56cf68d40a5f4ba7b',
-         'aid': -7,
-         'created': 1477433642,
-         'height': 549,
-         'owner_id': -101271420,
-         'pid': 436935371,
-         'post_id': 31289,
-         'src': 'https://pp.vk.me/c836338/v836338795/802a/J4uaRcNHVjU.jpg',
-         'src_big': 'https://pp.vk.me/c836338/v836338795/802b/624tc0jELKU.jpg',
-         'src_small': 'https://pp.vk.me/c836338/v836338795/8029/L0ghgGdL7dY.jpg',
-         'src_xbig': 'https://pp.vk.me/c836338/v836338795/802c/rKOqgxVH3uQ.jpg',
-         'text': '',
-         'user_id': 100,
-         'width': 807,
-         }
+        # {'access_key': 'd56cf68d40a5f4ba7b',
+        # 'aid': -7,
+        # 'created': 1477433642,
+        # 'height': 549,
+        # 'owner_id': -101271420,
+        # 'pid': 436935371,
+        # 'post_id': 31289,
+        # 'src': 'https://pp.vk.me/c836338/v836338795/802a/J4uaRcNHVjU.jpg',
+        # 'src_big': 'https://pp.vk.me/c836338/v836338795/802b/624tc0jELKU.jpg',
+        # 'src_small': 'https://pp.vk.me/c836338/v836338795/8029/L0ghgGdL7dY.jpg',
+        # 'src_xbig': 'https://pp.vk.me/c836338/v836338795/802c/rKOqgxVH3uQ.jpg',
+        # 'text': '',
+        # 'user_id': 100,
+        # 'width': 807,
+        # }
         fields = ('src_xxxbig', 'src_xxbig', 'src_xbig', 'src_big',
                   'src', 'src_small',
                   )
